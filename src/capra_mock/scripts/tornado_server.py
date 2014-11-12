8fb4f257c45a14a350e3b5c394aa92aa09759a69
Yohan <yohtm@hotmail.com>
2014-11-12 13:46:07 -0500

    comments for better challenge

--- src/capra_mock/scripts/tornado_server.py
+++ src/capra_mock/scripts/tornado_server.py
@@ -9,8 +9,21 @@ from geometry_msgs.msg import Twist
 def callback(data):
     print "Speed is currently " + str(data.linear.x)
 
+    ## Pour utiliser le handler ici, il faudra mettre les lignes suivantes dans son open():
+    # global handler
+    # handler = self
 
-#La node ROS
+    ## Cela le rendra accessible de partout de le code. Ce n'est pas une bonne pratique mais
+    ## c'est le plus simplque que je vois pour faire fonctionner ça ici. Pour l'utiliser, il
+    ## faudra faire:
+    # if "handler" in globals():
+    #   global handler
+    #   do stuff with handler
+
+
+
+## La node ROS. Lors de la réception d'un message sur le topic /mock_robot_node/mock_vel",
+## la fonction callback ci-haut est appelée
 class MockRobot:
 
     def __init__(self):
@@ -26,8 +39,9 @@ class MockRobot:
 
 if __name__ == "__main__":
 
-     # Implémenter l'initialisation ici. Vous allez devoir utiliser des threads pour que le code
-     # ne bloque pas à votre serveur
+     ## Implémenter l'initialisation de vos serveurs ici. Vous allez devoir utiliser des
+     ## threads pour que le code ne bloque pas à votre serveur et continue pour exécuter
+     ## la node ROS
 
     try:
         MockRobot()
